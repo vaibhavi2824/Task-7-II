001db04d2fcfc0223783e9378cbf54b3f6d65721
Vaibhavi Gurav <vaibhavigurav28052003@gmail.com>
2024-06-25 23:29:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Add event listeners to the buttons
+document.getElementById('login-btn').addEventListener('click', function(){
+    console.log('Login button clicked');
+});
+
+document.getElementById('register-btn').addEventListener('click', function(){
+    console.log('Register button clicked');
+});
